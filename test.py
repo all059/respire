664dd05c811436bc64d550d838079fc6f05c0b30
jian13579 <jhl3@williams.edu>
2020-05-17 10:53:05 -0400

    checking in changes

--- test.py
+++ test.py
@@ -4,11 +4,9 @@ import numpy
 import os
 from tensorflow import keras
 from tensorflow.keras import layers
+from keras.models import Sequential
 from scipy.io.wavfile import read
-from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
-
-a = read("audioset__3RvCwwIZ4w_10_15.wav")
-testArray = numpy.array(a[1], dtype=float)
+from tensorflow.keras.layers.experimental.preprocessing import Normalization
 
 ## standard convention
 batch_size = 32
@@ -18,32 +16,28 @@ progress = 0
 
 sickTraining = []
 for filename in os.listdir("c:/Users/warri/Desktop/respire/audio/audio/test/sick"):
-    progress += 1
-    print(progress)
     sickTraining.append("c:/Users/warri/Desktop/respire/audio/audio/test/sick/" + filename)
 
 not_sickTraining = []
 for notsickfile in os.listdir("c:/Users/warri/Desktop/respire/audio/audio/test/not_sick"):
-    progress += 1
-    print(progress)
     not_sickTraining.append("c:/Users/warri/Desktop/respire/audio/audio/test/not_sick/" + notsickfile)
 
+totalValidationArray = []
+totalValidationArrayLabels = []
 ## Convert validation data into arraylist of validation arrays
-sickTrainingConverted = []
 for file in sickTraining:
     a = read(file)
     numberArray = numpy.array(a[1], dtype=float)
-    sickTrainingConverted.append(numberArray)
-    progress += 1
-    print(progress)
+    # new_array = numpy.append(numberArray,1)
+    totalValidationArray.append(numberArray)
+    totalValidationArrayLabels.append(1)
 
-notSickTrainingConverted = []
 for file in not_sickTraining:
     a = read(file)
     numberArray = numpy.array(a[1], dtype=float)
-    notSickTrainingConverted.append(numberArray)
-    progress += 1
-    print(progress)
+    # new_array = numpy.append(numberArray,0)
+    totalValidationArray.append(numberArray)
+    totalValidationArrayLabels.append(0)
 
 # print("Yo im sick.")
 # print(sickTrainingConverted)
@@ -52,10 +46,41 @@ for file in not_sickTraining:
 # print(notSickTrainingConverted)
 
 ## preprocessing/ normalizing our data
-training_data = sickTrainingConverted
 
-vectorizer = TextVectorization(output_mode="int")
-vectorizer.adapt(training_data[0])
 
-training_data_shit = vectorizer(training_data)
-print(training_data)
+# normalizer = Normalization(axis=-1)
+# normalizer.adapt(sickTrainingConverted[0])
+# normalized_data = normalizer(sickTrainingConverted[0])
+# print("var: %.4f" % numpy.var(normalized_data))
+# print("mean: %.4f" % numpy.mean(normalized_data))
+padded_input = tf.keras.preprocessing.sequence.pad_sequences(totalValidationArray, padding='post')
+
+train_dataset = tf.data.Dataset.from_tensor_slices((padded_input, totalValidationArrayLabels))
+
+
+BATCH_SIZE = 64
+SHUFFLE_BUFFER_SIZE = 100
+
+train_dataset = train_dataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
+# test_dataset = test_dataset.batch(BATCH_SIZE)
+
+model = tf.keras.Sequential([
+    # tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10)
+])
+
+model.compile(optimizer=tf.keras.optimizers.RMSprop(),
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['sparse_categorical_accuracy'])
+
+model.fit(train_dataset, epochs=10)
+
+# model = keras.Sequential()
+# model.add(layers.Dense(12, input_dim = 200, activation='relu'))
+# model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# sizeOfArray = totalValidationArray[0].size
+# X = totalValidationArray[0][0:sizeOfArray-2]
+# Y = totalValidationArray[0][sizeOfArray-1]
+# model.fit(X, Y, epochs=100, batch_size=batch_size)
