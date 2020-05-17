96b9b4093c90e95a529f3104b72af066daf972c7
jian13579 <jhl3@williams.edu>
2020-05-17 12:09:32 -0400

    Created model, and added proper layering for our binary dataset

--- test.py
+++ test.py
@@ -2,11 +2,17 @@ import tensorflow as tf
 import scipy.io as sio
 import numpy
 import os
+import pandas as pd
 from tensorflow import keras
 from tensorflow.keras import layers
 from keras.models import Sequential
 from scipy.io.wavfile import read
 from tensorflow.keras.layers.experimental.preprocessing import Normalization
+
+
+## convert this to a jupyter notebook
+## TODO: test model on training data, test model on test data
+## get an accuracy we are happy with
 
 ## standard convention
 batch_size = 32
@@ -24,63 +30,81 @@ for notsickfile in os.listdir("c:/Users/warri/Desktop/respire/audio/audio/test/n
 
 totalValidationArray = []
 totalValidationArrayLabels = []
+MAX_SIZE = 300000
 ## Convert validation data into arraylist of validation arrays
 for file in sickTraining:
     a = read(file)
     numberArray = numpy.array(a[1], dtype=float)
-    # new_array = numpy.append(numberArray,1)
-    totalValidationArray.append(numberArray)
+    result = numpy.zeros(MAX_SIZE)
+    result[:numberArray.shape[0]] = numberArray
+    totalValidationArray.append(result)
     totalValidationArrayLabels.append(1)
+    progress+=1
+    print(progress)
 
 for file in not_sickTraining:
     a = read(file)
     numberArray = numpy.array(a[1], dtype=float)
-    # new_array = numpy.append(numberArray,0)
-    totalValidationArray.append(numberArray)
+    result = numpy.zeros(MAX_SIZE)
+    result[:numberArray.shape[0]] = numberArray
+    totalValidationArray.append(result)    
     totalValidationArrayLabels.append(0)
+    progress+=1
+    print(progress)
 
-# print("Yo im sick.")
-# print(sickTrainingConverted)
+print("converting array")
+totalValidationArray = numpy.array(totalValidationArray)
+totalValidationArrayLabels = numpy.array(totalValidationArrayLabels)
 
-# print("Yo im healthy.")
-# print(notSickTrainingConverted)
+print(totalValidationArray.shape)
+print(totalValidationArrayLabels)
 
-## preprocessing/ normalizing our data
-
-
-# normalizer = Normalization(axis=-1)
-# normalizer.adapt(sickTrainingConverted[0])
-# normalized_data = normalizer(sickTrainingConverted[0])
-# print("var: %.4f" % numpy.var(normalized_data))
-# print("mean: %.4f" % numpy.mean(normalized_data))
-padded_input = tf.keras.preprocessing.sequence.pad_sequences(totalValidationArray, padding='post')
-
-train_dataset = tf.data.Dataset.from_tensor_slices((padded_input, totalValidationArrayLabels))
-
-
-BATCH_SIZE = 64
-SHUFFLE_BUFFER_SIZE = 100
-
-train_dataset = train_dataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
-# test_dataset = test_dataset.batch(BATCH_SIZE)
+training_data_x = pd.DataFrame(data=totalValidationArray[0:1])
+print(training_data_x.shape)
 
 model = tf.keras.Sequential([
-    # tf.keras.layers.Flatten(input_shape=(28, 28)),
     tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(10)
+    tf.keras.layers.Dense(1, activation='sigmoid')
 ])
 
-model.compile(optimizer=tf.keras.optimizers.RMSprop(),
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['sparse_categorical_accuracy'])
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
 
-model.fit(train_dataset, epochs=10)
+print("starting training")
+model.fit(totalValidationArray, totalValidationArrayLabels, epochs=10, batch_size=batch_size)
 
-# model = keras.Sequential()
-# model.add(layers.Dense(12, input_dim = 200, activation='relu'))
-# model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model.evaluate(totalValidationArray, totalValidationArrayLabels))
 
-# sizeOfArray = totalValidationArray[0].size
-# X = totalValidationArray[0][0:sizeOfArray-2]
-# Y = totalValidationArray[0][sizeOfArray-1]
-# model.fit(X, Y, epochs=100, batch_size=batch_size)
+## TEST DATA
+sickTrainingTest = []
+for filename in os.listdir("c:/Users/warri/Desktop/respire/audio/audio/test/sick"):
+    sickTraining.append("c:/Users/warri/Desktop/respire/audio/audio/test/sick/" + filename)
+
+not_sickTrainingTest = []
+for notsickfile in os.listdir("c:/Users/warri/Desktop/respire/audio/audio/test/not_sick"):
+    not_sickTraining.append("c:/Users/warri/Desktop/respire/audio/audio/test/not_sick/" + notsickfile)
+
+totalValidationArray = []
+totalValidationArrayLabels = []
+MAX_SIZE = 300000
+## Convert validation data into arraylist of validation arrays
+for file in sickTraining:
+    a = read(file)
+    numberArray = numpy.array(a[1], dtype=float)
+    result = numpy.zeros(MAX_SIZE)
+    result[:numberArray.shape[0]] = numberArray
+    totalValidationArray.append(result)
+    totalValidationArrayLabels.append(1)
+    progress+=1
+    print(progress)
+
+for file in not_sickTraining:
+    a = read(file)
+    numberArray = numpy.array(a[1], dtype=float)
+    result = numpy.zeros(MAX_SIZE)
+    result[:numberArray.shape[0]] = numberArray
+    totalValidationArray.append(result)    
+    totalValidationArrayLabels.append(0)
+    progress+=1
+    print(progress)
