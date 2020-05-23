4701d4bd7734c79423def79e79f69f8f3820a62e
all059 <andylu059@gmail.com>
2020-05-23 01:35:41 -0400

    provided commentary/documentation on code in order to locate error in model

--- test.py
+++ test.py
@@ -19,9 +19,10 @@ from tensorflow.keras.layers.experimental.preprocessing import Normalization
 ## Standard convention for batch size; a batch size is the number of training examples used in one epoch
 batch_size = 32
 
-## Convert training data into arraylist of training arrays
+## Progress will be an int variable used to confirm that the for loops are running
 progress = 0
 
+## Meant to iterate through the training audio folders and append each .wav file into each list respectfully
 sickTraining = []
 for filename in os.listdir("c:/Users/warri/Desktop/respire/audio/audio/train/sick"):
     sickTraining.append("c:/Users/warri/Desktop/respire/audio/audio/train/sick/" + filename)
@@ -30,10 +31,17 @@ not_sickTraining = []
 for notsickfile in os.listdir("c:/Users/warri/Desktop/respire/audio/audio/train/not_sick"):
     not_sickTraining.append("c:/Users/warri/Desktop/respire/audio/audio/train/not_sick/" + notsickfile)
 
+## tVA will be the data coming from each wav file filled with 0's at the end
+## tVAL is 1 for sick, 0 for not sick
 totalValidationArray = []
 totalValidationArrayLabels = []
 MAX_SIZE = 300000
-## Convert validation data into arraylist of validation arrays
+
+## Convert training data into a list of training arrays
+## The read function from scipy reads wav files and returns an int and numpy array (samplerate, data)
+## The data returned will be set as numberArray, however this is a very big array
+## numpy.zeros returns an array of (shape, data type, order), our shape is 300,000
+## Result is used to find how many rows of data are in a EACH numberArray
 for file in sickTraining:
     a = read(file)
     numberArray = numpy.array(a[1], dtype=float)
@@ -54,6 +62,7 @@ for file in not_sickTraining:
     progress+=1
     print(progress)
 
+## list to array
 print("converting array")
 totalValidationArray = numpy.array(totalValidationArray)
 totalValidationArrayLabels = numpy.array(totalValidationArrayLabels)
@@ -61,6 +70,10 @@ totalValidationArrayLabels = numpy.array(totalValidationArrayLabels)
 print(totalValidationArray.shape)
 print(totalValidationArrayLabels)
 
+## tf.keras.Sequential(layers=None, name=None)
+## Sequential groups a linear stack of layers into a tf.keras.Model.
+## Model groups layers into an object with training and inference features.
+## https://tinyurl.com/yd9arn68 for info on relu and sigmoid
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dense(1, activation='sigmoid')
@@ -70,11 +83,12 @@ model.compile(optimizer='adam',
               loss='binary_crossentropy',
               metrics=['accuracy'])
 
+## .fit will train the data
 print("starting training")
 model.fit(totalValidationArray, totalValidationArrayLabels, epochs=5, batch_size=batch_size)
 print(model.evaluate(totalValidationArray, totalValidationArrayLabels))
-
-## Convert test data into arraylist of test arrays
+ 
+## Meant to iterate through the test audio folders and append each .wav file into each list respectfully
 sickTrainingTest = []
 for filename in os.listdir("c:/Users/warri/Desktop/respire/audio/audio/test/sick"):
     sickTraining.append("c:/Users/warri/Desktop/respire/audio/audio/test/sick/" + filename)
